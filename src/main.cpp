70527bc0114106f845381739fcdbcbe6128aa673
fongsu <0701289-6@g.puiching.edu.mo>
2019-03-06 12:14:44 +0800

    just a ordinary commit

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,13 @@
 #include <boost/thread/thread.hpp>
 #include <pcl/io/pcd_io.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/statistical_outlier_removal.h>
 #include "../visualizer/visualizer.h"
 
 int main() {
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_stage_1(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_stage_2(new pcl::PointCloud<pcl::PointXYZ>);
 
     pcl::io::loadPCDFile<pcl::PointXYZ>("../prefabs/table_scene_lms400.pcd", *cloud);
 
@@ -19,19 +21,27 @@ int main() {
     pcl::VoxelGrid<pcl::PointXYZ> sor;
     sor.setInputCloud(cloud);
     sor.setLeafSize(0.01f, 0.01f, 0.01f);
-    sor.filter(*cloud_filtered);
+    sor.filter(*cloud_stage_1);
 
-    std::cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height
-              << " data points (" << pcl::getFieldsList(*cloud_filtered) << ").";
+    pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor2;
+    sor2.setInputCloud(cloud_stage_1);
+    sor2.setMeanK(50);
+    sor2.setStddevMulThresh(1.0);
+    sor2.filter(*cloud_stage_2);
 
 
-    auto viewer = twoViewportsBW(cloud, cloud_filtered);
+    std::cerr << "PointCloud after filtering: " << cloud_stage_1->width * cloud_stage_1->height
+              << " data points (" << pcl::getFieldsList(*cloud_stage_1) << ").";
+    std::cerr << "PointCloud after filtering: " << cloud_stage_2->width * cloud_stage_2->height
+              << " data points (" << pcl::getFieldsList(*cloud_stage_2) << ").";
+
+    auto viewer = twoViewportsBW(cloud_stage_1, cloud_stage_2);
 
     while(!viewer->wasStopped())
     {
         viewer->spinOnce(100);
-        boost::this_thread::sleep (boost::posix_time::microseconds(100000));
+        boost::this_thread::sleep (boost::posix_time::microseconds(10000));
+        std::cout << "HAHAHAHA" << endl;
     }
-    //.jlpqwefpoipoij
 }
 
--- visualizer/visualizer.h
+++ visualizer/visualizer.h
@@ -13,12 +13,14 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/visualization/pcl_visualizer.h>
 
-pcl::visualization::PCLVisualizer::Ptr twoViewports(
+//*************function declarations*****************//
+
+pcl::visualization::PCLVisualizer::Ptr twoViewports(  //render two PointXYZRGB PointCloud together
         pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr,
         pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr);
 
-pcl::visualization::PCLVisualizer::Ptr simpleVis (pcl::PointCloud<pcl::PointXYZ>::ConstPtr);
-pcl::visualization::PCLVisualizer::Ptr twoViewportsBW(
+pcl::visualization::PCLVisualizer::Ptr simpleVis (pcl::PointCloud<pcl::PointXYZ>::ConstPtr);   //render a single PoinxXYZ PointCloud
+pcl::visualization::PCLVisualizer::Ptr twoViewportsBW(  //render two PointXYZ Pointcloud Together
         pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud,
         pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud2);
 #endif //PCL_REAL_VISUALIZER_H

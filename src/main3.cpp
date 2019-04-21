d3920ec4ffdc64583e3b5dd0902394bedc818282
fongsu <0701289-6@g.puiching.edu.mo>
2019-04-21 21:47:24 +0800

    Merge branch 'OLIVER_VMWARE' of /home/fongsu/Workspace/PCL_REAL with conflicts.

--- src/main3.cpp
+++ src/main3.cpp
@@ -6,7 +6,6 @@
 #include <vector>
 #include <thread>
 #include <imgui.h>
-#include <imgui_impl_glfw.h>
 
 #include <librealsense2/rs.hpp>
 #include <example.hpp>
@@ -32,7 +31,7 @@ int main()
 {
 	rs2::log_to_console(RS2_LOG_SEVERITY_ERROR);
 	window app(1280, 720, "Realsense Capture");
-	ImGui_ImplGlfw_Init(app, false);
+	Imgui_ImplGlfw_Init(app, false);
 
 	rs2::colorizer color_map;
 	rs2::rates_printer printer;
@@ -45,34 +44,12 @@ int main()
 	//std::thread distanceDisplayThread(distanceDisplay, std::ref(data));
 
 	char t;
-	bool my_panel_activate = false;
 	while (app) {
 		data = gen.wait_for_frames().apply_filter(printer).apply_filter(color_map);
 		app.show(data);
-//    	if(!scanf("%c", &t))
-//	    {
-//		    continue;
-//	    } else{
-//		    if(t == 'h')
-//			    gen.Gen_compute();
-//		    else if (t == 'q')
-//			    break;
-//    	}
-//		static const int flags = ImGuiWindowFlags_NoCollapse
-//		                         | ImGuiWindowFlags_NoScrollbar
-//		                         | ImGuiWindowFlags_NoSavedSettings
-//		                         | ImGuiWindowFlags_NoTitleBar
-//		                         | ImGuiWindowFlags_NoResize
-//		                         | ImGuiWindowFlags_NoMove;
-		ImGui_ImplGlfw_NewFrame(1);
-		ImGui::Begin("Panel", &my_panel_activate);
-		if(ImGui::Button("Capture", {50, 50}))
-		{
-			gen.Gen_compute();
-		}
-		ImGui::Text("Distance from the middle : %f", gen.midDist());
-		ImGui::End();
-		ImGui::Render();
+
+		//Render the UI
+		Img
     }
 	//if(distanceDisplayThread.joinable())
 	//	distanceDisplayThread.join();
